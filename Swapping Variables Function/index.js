244f9a6e9ac437cf56a7d3e673b6eaf09bebd01f
abdalla12345678 <a.ahmed24@share.epsb.ca>
2018-09-25 13:12:00 -0600

    changes

---
+++ Swapping Variables Function/index.js
@@ -0,0 +1,33 @@
+let MarlMercer = [];
+let MeganSpence = [];
+
+
+MarkMercer = ["Mark, "Mercer", 13, 29, null, true, false];
+MeganSpence = ["Megan", "Spence", 14, null, true, false, true];
+
+
+console.log(MarkMercer);
+console.log(MeganSpence);
+
+fuctiom square (number) {
+  returm*numer;
+}
+console.log("Last Name is: " + MarkMercer [1]);
+console.log(Last Name is: " + MeganSpence[1]);
+
+MarkMercer[1]="Is Awesome";
+console.log(MarkMercer);
+
+{
+  firstName: ,
+  lastName: ,
+  age: ,
+  ageAfraid: ,
+  hair: ,
+  male: ,
+  female: ,
+}
+
+let MeganSpence = {
+
+}
---
+++ Variables Object Start/index.js
@@ -0,0 +1,31 @@
+let MarlMercer = [];
+let MeganSpence = [];
+
+
+MarkMercer = ["Mark, "Mercer", 13, 29, null, true, false];
+MeganSpence = ["Megan", "Spence", 14, null, true, false, true];
+
+
+console.log(MarkMercer);
+console.log(MeganSpence);
+
+
+console.log("Last Name is: " + MarkMercer [1]);
+console.log(Last Name is: " + MeganSpence[1]);
+
+MarkMercer[1]="Is Awesome";
+console.log(MarkMercer);
+
+{
+  firstName: ,
+  lastName: ,
+  age: ,
+  ageAfraid: ,
+  hair: ,
+  male: ,
+  female: ,
+}
+
+let MeganSpence = {
+
+}
