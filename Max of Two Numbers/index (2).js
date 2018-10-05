084bcf881e362a308cdf512245d0d522b36915f9
abdalla12345678 <a.ahmed24@share.epsb.ca>
2018-10-05 13:58:55 -0600

    new

---
+++ Max of Two Numbers/index (2).js
@@ -0,0 +1,13 @@
+let numberA= 8; //change number here to any value you desire
+let numberB=5;  //change number here to any value you desire
+
+if (numberA > numberB)
+console.log(numberA, "is bigger.");// says this if number A is bigger
+else if
+ (numberA < numberB) console.log(numberB, "is bigger."); //says this if numberB is bigger
+else
+ console.log("The numbers are the same."); //prints this if number values are the same
+
+console.log(numberA); //prints the value of numberA to the console
+
+console.log(numberB); //prints the value of numberB to the console
