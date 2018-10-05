895a7f230c858f6b9865387789d17d83327832df
abdalla12345678 <a.ahmed24@share.epsb.ca>
2018-10-05 13:46:36 -0600

    change

---
+++ Max of Two Numbers/README.md
@@ -0,0 +1,25 @@
+# Max of Two Numbers Assignment
+
+**Under Construction**
+
+Problem Situation -
+
+Output to the Console
+- 
+
+Case Study Details
+-
+
+Developing the Algorithm
+-
+
+Starter Code to Consider
+-
+
+---
+
+# Ideas
+Include index.html & index.js
+
+
+---
---
+++ Max of Two Numbers/index.js
@@ -0,0 +1,10 @@
+let numberA= 7;
+let numberB= 5;
+
+if (numberA > numberB) big = numberA;
+console.log("The bigger number is", numberA);
+else if (numberA < numberB) big = numberB
+console.log("The bigger number is", numberB);
+else console.log("The numbers are the same");
+
+console.log("The biggest is");
