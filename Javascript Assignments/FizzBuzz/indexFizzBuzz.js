497d73f7d4327a051245b50ce55d06dfc49249fc
abdalla12345678 <a.ahmed24@share.epsb.ca>
2018-10-25 13:17:17 -0600

    changes + new

--- Javascript Assignments/FizzBuzz/indexFizzBuzz.js
+++ Javascript Assignments/FizzBuzz/indexFizzBuzz.js
@@ -1,13 +1,7 @@
-if(isNaN (value) ) {
-  stop = true;
-  return "Type a real Number"
-} else {
-  return "Input Validate"
-}
 
+let a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = 'value'
 let one = 'value'
-let a = 'value'
-const testValue = 90 ;
+const testValue = 90 
 console.log(fizzBuzz(testValue)); //Calling Function
 
 function fizzBuzz (input) { //These match order of logic
